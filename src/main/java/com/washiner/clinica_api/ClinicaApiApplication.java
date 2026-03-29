a85168851a1a89a5bb73fb388912af2b6f3e93d9
Washiner <washiner@gmail.com>
2026-03-29 21:08:31 +0900

    chore: configuracao inicial do projeto com docker e banco de dados

---
+++ src/main/java/com/washiner/clinica_api/ClinicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.washiner.clinica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/washiner/clinica_api/ClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washiner.clinica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
